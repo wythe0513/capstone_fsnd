6e7178876cb3eea217d2e4671bdea46455f34c97
wythe0513 <wythenshawe0513@gmail.com>
2021-04-17 22:48:12 +0900

    initialize

---
+++ models.py
@@ -0,0 +1,45 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String
+from flask_sqlalchemy import SQLAlchemy
+import json
+import os
+
+database_path = os.environ.get('DATABASE_URL')
+#if not database_path:
+#    database_name = "test_heroku_app"
+#    database_path = "postgresql://{}/{}".format('postgres:wythenshawe0513@localhost:5432', database_name)
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+'''
+Person
+Have title and release year
+'''
+class Person(db.Model):  
+  __tablename__ = 'People'
+
+  id = Column(Integer, primary_key=True)
+  name = Column(String)
+  catchphrase = Column(String)
+
+  def __init__(self, name, catchphrase=""):
+    self.name = name
+    self.catchphrase = catchphrase
+
+  def format(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'catchphrase': self.catchphrase}
---
+++ setup.sh
@@ -0,0 +1,2 @@
+set DATABASE_URL = "postgresql://postgres:wythenshawe0513@localhost:5432/test_heroku_app"
+set EXCITED = 'true'
